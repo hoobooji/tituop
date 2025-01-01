07db05b1e2ba13c0a3735cf65aba0ff2cf5f222d
rohit8708913 <koyeb67user@gmail.com>
2025-01-01 18:19:50 +0530

    Update FORMATS.py

--- plugins/FORMATS.py
+++ plugins/FORMATS.py
@@ -3,6 +3,9 @@ off_pic = "https://telegra.ph/file/0d9e590f62b63b51d4bf9.jpg"
 files_cmd_pic = "https://telegra.ph/file/d44f46054250a73053614.jpg"
 autodel_cmd_pic = "https://telegra.ph/file/a64533814021b40057ccd.jpg"
 
+
+SET_SHORTENER_CMD_TXT = "Shortener is {shortener_status}. Use `/setshortener <url> <api_key>` to configure the shortener."
+
 #start message
 START_MSG = """Hello {mention}
 
