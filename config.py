4764249505266032e49a49b3921006ad02da5942
rohit8708913 <koyeb67user@gmail.com>
2025-01-07 23:41:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,9 +46,7 @@ QR_PIC = os.environ.get("QR_PIC", "https://envs.sh/B7w.png")
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 
-
-#Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>ʙʏ @Javpostr</b>")
