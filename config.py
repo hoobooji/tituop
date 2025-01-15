62aa5f745fcfc6efc2baf681a778c7096eacd5c9
rohit8708913 <koyeb67user@gmail.com>
2025-01-15 16:26:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,13 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7542241757:AAG-Qx8U60UEQzB-3JF7ssayN8I4mkJp3eo")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "9698652"))
+API_ID = int(os.environ.get("API_ID", ""))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "b354710ab18b84e00b65c62ba7a9c043")
+API_HASH = os.environ.get("API_HASH", "")
 
 #Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002008354608"))
@@ -26,9 +26,11 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "7328629001"))
 #Port
 PORT = os.environ.get("PORT", "6726")
 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://koyeb77user:rohit870@cluster0.wgdkp.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "cphdlust1234")
-
+
+
+
 
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/delight_link/2")
@@ -44,7 +46,8 @@ QR_PIC = os.environ.get("QR_PIC", "https://envs.sh/B7w.png")
 
 
 
-
+
+
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>ʙʏ @Javpostr</b>")
@@ -92,7 +95,8 @@ PRICE4 = os.environ.get("PRICE4", "280 rs")
 
 #1 Year
 PRICE5 = os.environ.get("PRICE5", "550 rs")
-
+
+
 #===================(END)========================#
 
 
@@ -102,7 +106,8 @@ DISABLE_CHANNEL_BUTTON = os.environ.get("True", True) == 'True'
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Sharing Bot!"
 
-
+
+
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
